0dd963a66a922a144588f4e9cceeeeb81e2e534c
mahdi <mahdi.mnm77@gmail.com>
2022-07-30 14:47:52 +0430

    added_function_ws_and_vp

--- Free_water/free_water.py
+++ Free_water/free_water.py
@@ -1,12 +1,16 @@
 import Mean_Temperature_max_min
 import Solar_or_shortwave_radiation
 import Slope_Vapour_Pressure_Curve_With_Maen_Temperature as slope
+import Wind_speed_at_2m_above_ground_surface
+import Saturation_Vapour_Pressure_With_Temperature
+import Actual_Vapour_Pressure
 
 class E_free_water : 
 
      # init method or constructor 
     def __init__(self,
         method_free_water_based_on_radiation : str = None,
+        method_ws_and_vp : str = None,
         T_max : float = None,
         T_min : float = None,
         T_mean : float = None,
@@ -21,9 +25,25 @@ class E_free_water :
         Adjustment_coefficient_or_K_RS : float = None,
         R_S : float = None,
         method_Solar_or_shortwave_radiation : str = None,
-        latent_heat_of_vaporization : float = None
+        latent_heat_of_vaporization : float = None,
+        water_area_of_a_lake_or_reservoir : float = None,
+        wind_speed_at_2m : float = None,
+        e_s : float = None,
+        e_a : float = None,
+        Altitude_at_which_wind_speed_is_measured : float = None, 
+        Measured_wind_speed : float = None,
+        method_Actual_Vapour_Pressure : str = None,
+        T_dew : float  = None,
+        RH_max : float  = None,
+        RH_min : float  = None,
+        RH_mean : float  = None,
+        T_wet : float  = None,
+        T_dry : float  = None,
+        a_psy : float  = None,
+        Altitude : float = None
     ):
         self.method_free_water_based_on_radiation = method_free_water_based_on_radiation
+        self.method_ws_and_vp = method_ws_and_vp
         self.T_max = T_max
         self.T_min = T_min
         self.T_mean= T_mean
@@ -39,7 +59,21 @@ class E_free_water :
         self.R_S = R_S
         self.method_Solar_or_shortwave_radiation = method_Solar_or_shortwave_radiation
         self.latent_heat_of_vaporization = latent_heat_of_vaporization
-
+        self.water_area_of_a_lake_or_reservoir = water_area_of_a_lake_or_reservoir
+        self.wind_speed_at_2m = wind_speed_at_2m
+        self.e_s = e_s
+        self.e_a = e_a
+        self.Altitude_at_which_wind_speed_is_measured = Altitude_at_which_wind_speed_is_measured
+        self.Measured_wind_speed = Measured_wind_speed
+        self.method_Actual_Vapour_Pressure = method_Actual_Vapour_Pressure
+        self.T_dew = T_dew
+        self.RH_max = RH_max
+        self.RH_min = RH_min
+        self.RH_mean = RH_mean
+        self.T_wet = T_wet
+        self.T_dry = T_dry
+        self.a_psy = a_psy
+        self.Altitude = Altitude
    
     # Sample Method 
     def based_on_radiation(
@@ -188,9 +222,108 @@ class E_free_water :
         
         return 52.6 * (delta / (delta + latent_heat_of_vaporization)) * (R_S / latent_heat_of_vaporization) - 0.12 
 
+
+    def based_on_wind_speed_and_vapor_pressure(
+        method_ws_and_vp : str,
+        water_area_of_a_lake_or_reservoir : float,
+        wind_speed_at_2m : float = None,
+        e_s : float = None,
+        e_a : float = None,
+        Altitude_at_which_wind_speed_is_measured : float = None, 
+        Measured_wind_speed : float = None,
+        T_max : float = None,
+        T_min : float = None,
+        method_Actual_Vapour_Pressure : str = None,
+        T_dew : float  = None,
+        T_mean : float  = None,
+        RH_max : float  = None,
+        RH_min : float  = None,
+        RH_mean : float  = None,
+        T_wet : float  = None,
+        T_dry : float  = None,
+        a_psy : float  = None,
+        Altitude : float = None
+    )-> float:
     
-    
-    
+        """
+        Description
+        -----------
+        calculate Actual Vapour Pressure With Dewpoint temperature - eq 14 FAO56
+        ----------
+        Dewpoint temperature : float
+            Dewpoint temperature in celsius
+
+        Returns
+        -------
+        Actual Vapour Pressure : float
+            Actual Vapour Pressure in Kilo pascal
+        """
+        if e_s is None :
+            e_s = total_saturation_vapour_pressure(
+                T_max = T_max,
+                T_min = T_min
+            )
+        else:
+            e_s = e_s
+        
+
+        if e_a is None :
+            if method_Actual_Vapour_Pressure == 'dew':
+                e_a = Actual_Vapour_Pressure.Actual_Vapour_Pressure.dew(
+                    T_dew = T_dew
+                )
+
+            elif method_Actual_Vapour_Pressure == 'RH_and_T_max_min':
+                e_a = Actual_Vapour_Pressure.Actual_Vapour_Pressure.RH_and_T_max_min(
+                    T_max = T_max,
+                    T_min = T_min,
+                    RH_max = RH_max,
+                    RH_min = RH_min
+                )
+
+            elif method_Actual_Vapour_Pressure == 'RH_max_and_T_min':
+                e_a = Actual_Vapour_Pressure.Actual_Vapour_Pressure.RH_max_and_T_min(
+                    T_min = T_min,
+                    RH_max = RH_max
+                )
+
+            elif method_Actual_Vapour_Pressure == 'RH_mean_T_max_min':
+                e_a = Actual_Vapour_Pressure.Actual_Vapour_Pressure.RH_mean_T_max_min(
+                    T_max =T_max,
+                    T_min = T_min,
+                    RH_max = RH_max,
+                    RH_min = RH_min,
+                    RH_mean = RH_mean
+                )
+
+            elif method_Actual_Vapour_Pressure == 'T_wet_T_dry':
+                e_a = Actual_Vapour_Pressure.Actual_Vapour_Pressure.T_wet_T_dry(
+                    T_wet = T_wet,
+                    T_dry = T_dry,
+                    a_psy = a_psy,
+                    Altitude = Altitude
+                )
+            else:
+                e_a = e_a
+
+        e_s_minus_e_a = e_s - e_a
+
+        if wind_speed_at_2m is None : 
+            wind_speed_at_2m = Wind_speed_at_2m_above_ground_surface.wind_speed_at_2m_above_ground_surface(
+                Altitude_at_which_wind_speed_is_measured = Altitude_at_which_wind_speed_is_measured, 
+                Measured_wind_speed = Measured_wind_speed
+            )
+        else:
+            wind_speed_at_2m = wind_speed_at_2m
+        
+        if method_ws_and_vp == 'Harbeck' :
+            E = 2.909 * (water_area_of_a_lake_or_reservoir**(-0.05)) * wind_speed_at_2m * e_s_minus_e_a
+        elif method_ws_and_vp == 'Shuttleworth' :
+            E = 3.623 * (water_area_of_a_lake_or_reservoir**(-0.066)) * wind_speed_at_2m * e_s_minus_e_a
+        
+
+        return E
+
 
 
 
